1f9c728ad512bdbed6d7e34fa7a647fc7662caf2
MaiaJrDev <rm.dcjr@hotmail.com>
2022-03-07 15:16:06 -0300

    Rotas

---
+++ pokedex/src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,9 @@
+
+
+
+
+export default function ErrorPage(){
+    return(
+        <h1>Erro Pagina não encontrada</h1>
+    )
+}
---
+++ pokedex/src/pages/Pokedex/Pokedex.jsx
@@ -0,0 +1,7 @@
+
+
+export default function Pokedex(){
+    return(
+        <h1>Pokedex</h1>
+    )
+}
