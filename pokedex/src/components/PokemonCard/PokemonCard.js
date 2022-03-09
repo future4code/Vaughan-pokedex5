e519ac38d077bf23154af86575bd0c09655b5170
mdudapires <90624608+mdudapires@users.noreply.github.com>
2022-03-09 01:18:11 -0300

    cards

--- pokedex/src/components/PokemonCard/PokemonCard.js
+++ pokedex/src/components/PokemonCard/PokemonCard.js
@@ -1,26 +1,39 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
-import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
+import styled from 'styled-components';
 
+const CardContainer = styled.div`
+  border: 1px solid #66a3ff;
+  border-radius: 10px;
+  box-shadow: 0px 2px 10px black;
+  width: 300px;
+  margin-bottom: 10px;
+  margin-left: 10px;
+`;
+
+const CardMedia = styled.img`
+width: 100%;
+height: 250px;
+`;
+
+const AlignText = styled.h3`
+text-align: center;
+`
 
 export default function PokemonCard(props) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <CardContainer>
       <CardMedia
         component="img"
-        alt="green iguana"
         height="200"
-        image={props.img}
-        name={props.name}
-        typography
-      />
-      <h3>{props.name}</h3>
+        src={props.img}
+        />
+      <AlignText>{props.name}</AlignText>
       <CardActions>
         <Button size="small" variant={"contained"}>Adicionar a Pokedex</Button>
         <Button size="small" variant={"contained"}>Ver detalhes</Button>
       </CardActions>
-    </Card>
+    </CardContainer>
   );
 }
